f44b072fafbb2a07d469f485e4c15140da43f0dc
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-12-21 00:23:02 +0600

    build commit

--- lib/screens/all_contacts_screen.dart
+++ lib/screens/all_contacts_screen.dart
@@ -61,23 +61,34 @@ class _AllContactsScreenState extends ConsumerState<AllContactsScreen> {
             future: _userProfileFuture,
             builder: (BuildContext context,
                 AsyncSnapshot<DocumentSnapshot> snapshot) {
-              if (snapshot.connectionState == ConnectionState.done &&
-                  snapshot.hasData) {
-                Map<String, dynamic> userData =
-                    snapshot.data!.data() as Map<String, dynamic>;
-                String profileImageUrl =
-                    userData['profileImageUrl'] ?? 'default_fallback_image_url';
-                return GestureDetector(
-                  onTap: () {
-                    _showProfileOptions(context);
-                  },
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(profileImageUrl),
-                  ),
-                );
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData && snapshot.data!.data() != null) {
+                  Map<String, dynamic> userData =
+                      snapshot.data!.data() as Map<String, dynamic>;
+                  String profileImageUrl = userData['profileImageUrl'] ??
+                      'default_placeholder_image_url';
+                  return GestureDetector(
+                    onTap: () {
+                      _showProfileOptions(context);
+                    },
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(profileImageUrl),
+                      onBackgroundImageError: (exception, stackTrace) {},
+                      backgroundColor: Colors.transparent,
+                    ),
+                  );
+                } else {
+                  // User data is not available or userData is null
+                  return CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Favatar_3541871&psig=AOvVaw3Gs-H12-u5tD8pKi6juR3Q&ust=1702489575739000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOiK1oi6ioMDFQAAAAAdAAAAABAD'),
+                    backgroundColor: Colors.blue,
+                  );
+                }
+              } else {
+                // Show a placeholder or loading indicator while waiting for the data
+                return const CircleAvatar(child: CircularProgressIndicator());
               }
-              // Show a placeholder or loading indicator while waiting for the data
-              return const CircleAvatar(child: CircularProgressIndicator());
             },
           ),
           const SizedBox(width: 10),
